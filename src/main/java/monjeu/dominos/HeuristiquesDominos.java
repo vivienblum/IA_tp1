ea5607577b9c1063bacca82248a8257f9920ef76
Vivien Blum <vblum@a-018818.ups.u-psud.fr>
2017-09-21 08:49:46 +0200

    merge

--- src/main/java/monjeu/dominos/HeuristiquesDominos.java
+++ src/main/java/monjeu/dominos/HeuristiquesDominos.java
@@ -8,9 +8,10 @@ import jeux.modele.joueur.Joueur;
 public class HeuristiquesDominos{
 
 	public static  Heuristique hblanc = new Heuristique(){
-				
+		
 		public int eval(PlateauJeu p, Joueur j){
 			/*A COMPLETER*/
+			System.out.println("BLANC");
 			return 0;
 		}
 	};
@@ -19,6 +20,7 @@ public class HeuristiquesDominos{
 	
 		public int eval(PlateauJeu p, Joueur j){
 			/*A COMPLETER*/
+			System.out.println("NOIR");
 			return 0;
 		}
 	};
